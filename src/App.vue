d5af2a4b6cf602b4b350c721a6fa8f84f4fc40fd
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2025-01-22 00:26:41 +0900

    Add changes to project files

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <v-container>
         <FizzBuzz :numbers="[1, 2, 3, 4, 5, 15, 30]" />
         <FizzBuzz :numbers="[6, 7, 8, 9, 10, 11, 12]" />
+        <RockPaperScissors />
       </v-container>
     </v-main>
   </v-app>
@@ -11,6 +12,7 @@
 
 <script setup lang="ts">
 import FizzBuzz from './components/FizzBuzz.vue'
+import RockPaperScissors from './components/RockPaperScissors.vue'
 </script>
 
 <style>
